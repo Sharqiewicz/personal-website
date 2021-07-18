560eece7f125dab04e3bc1eb1cd759c8542c3bd3
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2021-07-18 16:16:09 +0200

    implement home section background

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import Navbar from './home/Navbar';
+import styles from '../../styles/Home.module.scss';
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <Navbar />
         </div>
     )
