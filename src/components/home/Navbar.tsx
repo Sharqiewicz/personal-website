19f18fe9b556e0996b3a811e862e99beda9f1eb9
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2021-07-18 13:48:32 +0200

    implement toggling home navbar

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../../../styles/Home.module.scss'
+
 import Github from '../../../public/icons/github.svg';
 import Instagram from '../../../public/icons/instagram.svg';
 import Linkedin from '../../../public/icons/linkedin.svg';
 import Youtube from '../../../public/icons/youtube.svg';
 
-import styles from '../../../styles/Home.module.scss'
+import Logo from '../../../public/socials-logo.svg';
+
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState<boolean>(true);
+
+    const openMenuStyles = `${styles["navbar__container"]} ${styles["navbar--open"]}`;
+    const closedMenuStyles = `${styles["navbar__container"]} ${styles["navbar--closed"]}`;
+
+
+    function toggleNavbar(event: React.MouseEvent<HTMLDivElement>) {
+        setIsOpen(state => !state);
+    }
+
     return (
-        <div className={styles["navbar__container"]}>
+        <div className={isOpen ? openMenuStyles : closedMenuStyles}>
             <div className={styles["navbar__menu"]}>
                 <div className={styles["navbar__group"]}>
-                    <div className={styles.dash + " " + styles["dash--red"]}></div>
+                    <div onClick={toggleNavbar} className={styles.dash + " " + styles["dash--red"]}></div>
                     <div className={styles.dash + " " + styles["dash--yellow"]}></div>
                     <div className={styles.dash + " " + styles["dash--green"]}></div>
                 </div>
@@ -22,6 +36,7 @@ export default function Navbar() {
                 <a className={styles['socials__link']} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kacperszarkiewicz/"><Linkedin /></a>
                 <a className={styles['socials__link']} target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCLJiABSd-uulY0TNv1wLJaA"><Youtube /></a>
             </div>
+            <Logo className={styles['navbar__logo']} onClick={toggleNavbar} />
         </div>
     )
 }
