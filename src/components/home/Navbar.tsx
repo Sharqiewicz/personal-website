c77318acf689745d517ba064e0458c66acf0592e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2021-07-18 11:05:20 +0200

    implement sttles of the home navbar

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -8,19 +8,19 @@ import styles from '../../../styles/Home.module.scss'
 
 export default function Navbar() {
     return (
-        <div className="navbar__container">
-            <div className="navbar__menu">
-                <div className="navbar__group">
+        <div className={styles["navbar__container"]}>
+            <div className={styles["navbar__menu"]}>
+                <div className={styles["navbar__group"]}>
                     <div className={styles.dash + " " + styles["dash--red"]}></div>
                     <div className={styles.dash + " " + styles["dash--yellow"]}></div>
                     <div className={styles.dash + " " + styles["dash--green"]}></div>
                 </div>
             </div>
-            <div className="navbar__socials">
-                <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/sharqiewicz/"><Instagram /></a>
-                <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://github.com/Sharqiewicz"><Github /></a>
-                <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kacperszarkiewicz/"><Linkedin /></a>
-                <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCLJiABSd-uulY0TNv1wLJaA"><Youtube /></a>
+            <div className={styles["navbar__socials"]}>
+                <a className={styles['socials__link']} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/sharqiewicz/"><Instagram /></a>
+                <a className={styles['socials__link']} target="_blank" rel="noopener noreferrer" href="https://github.com/Sharqiewicz"><Github /></a>
+                <a className={styles['socials__link']} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kacperszarkiewicz/"><Linkedin /></a>
+                <a className={styles['socials__link']} target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCLJiABSd-uulY0TNv1wLJaA"><Youtube /></a>
             </div>
         </div>
     )
